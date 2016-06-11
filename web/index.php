7892db2ff4875fcae480144cdcee16675c795152
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-06-11 12:29:10 -0300

    Refactorin routes and including twig extension

--- web/index.php
+++ web/index.php
@@ -2,12 +2,28 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Pprobat\Controller\HomeControllerProvider;
+use Pprobat\Controller\MemberControllerProvider;
+use Pprobat\Controller\MeetupControllerProvider;
+use Pprobat\Controller\AboutControllerProvider;
+
+use Pprobat\Twig\Extension\Bootstrap;
+
+use Pprobat\Form\Type\MemberType;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// -- Providers
 $app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__ . '/../views'
-])->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../view',
+    'twig.templates' => ['bootstrap_3_layout.html.twig']
+])->extend('twig', function($twig){
+    $twig->addExtension(new Bootstrap());
+    return $twig;
+});
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'drive' => 'pdo_mysql',
         'host' => '127.0.0.1',
@@ -16,23 +32,19 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
         'password' => 'pprobat'
     ),
 ));
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.messages' => array(),
+    'locale' => 'pt_BR'
+));
+$app->register(new Silex\Provider\FormServiceProvider(), []);
+$app['form.types'] = $app->extend('form.types', function ($types) use ($app) {
+    $types[] = new MemberType();
 
-$app->get('/', function() use ($app) {
-    return $app['twig']->render('home.html.twig', []);
-})->bind('index');
+    return $types;
+});
 
-$app->get('/members', function() use ($app){
-    return $app['twig']->render('members.html.twig', [
-        'members' => $app['db']->fetchAll('SELECT name FROM user')
-    ]);
-})->bind('get_members');
-
-$app->get('/meetups', function() use ($app){
-    return $app['twig']->render('meetups.html.twig', []);
-})->bind('get_meetups');
-
-$app->get('/about', function() use ($app){
-    return $app['twig']->render('about.html.twig', []);
-})->bind('get_about');
-
-$app->run();
+$app->mount('/', new HomeControllerProvider())
+    ->mount('/members', new MemberControllerProvider())
+    ->mount('/meetups', new MeetupControllerProvider())
+    ->mount('/about', new AboutControllerProvider())
+    ->run();
